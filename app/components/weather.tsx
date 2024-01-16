b8555f1ca099015b35a17e3057c22be37b81464f
Wan Muzaffar <wanmuz@Wans-MacBook-Air.local>
2024-01-16 15:01:48 +0700

    first commit

--- app/components/weather.tsx
+++ app/components/weather.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 
-export default function Weather() {
-  return (
-    <div>
-        <h1>Weather
-        </h1>
-        </div>
-  )
+interface WeatherProp {
+    weatherData: any
 }
+
+const Weather:React.FC<WeatherProp> = ({weatherData}) => {
+  return (
+   <div>
+    {
+        // the bracket here is needed because i m doing conditional rendering on a return
+        weatherData != null ? (
+<>
+    <h2>Weather for {weatherData.name}</h2>
+    <p>Temperature: {Math.round(weatherData.main.temp - 273.15)}</p>
+</>
+        )
+        :
+         // the bracket here is needed because i m doing conditional rendering on a return
+        (
+        <>
+        <p>No data available</p>
+        </>
+        )
+    }
+   </div>
+  );
+}
+
+export default Weather
--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,18 @@
 import Search from "./components/search"
 import Weather from "./components/weather"
 import axios from "axios"
+import { useState } from "react";
  
 export default function Home() {
+
+  const [weatherData, setWeatherData] = useState(null);
   const getWeather = (city:string) => {
     console.log(`${city} from page`)
     
     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9fd7a449d055dba26a982a3220f32aa2`)
     .then(response => {
       console.log(response.data);
+      setWeatherData(response.data);
     })
     .catch(error => {
       console.error('Error:', error);
@@ -20,7 +24,7 @@ export default function Home() {
   return (
     <main>
       <Search loadWeather={getWeather}/>
-      <Weather/>
+      <Weather weatherData={weatherData}/>
      
     </main>
   )
